f901803a1691802c14ac3d2459932ccaec9b5efc
Rails Test App <faisallarai@gmail.com>
2019-01-16 09:33:55 +0000

    changes

--- images/serializers.py
+++ images/serializers.py
@@ -5,4 +5,4 @@ from .models import Image
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
-        fields = ('title', 'image', 'width', 'heigth', 'size')
+        fields = ('title', 'image', 'width', 'height', 'size')
